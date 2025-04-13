e8d7a40648fc98240a0d49b5d8a16ca1ed69046d
mjml2411 <mjml2411@outlook.com>
2025-04-13 17:13:27 +0100

    not tested: updated script to make empty dirs and README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 # add the shebang (see https://en.wikipedia.org/wiki/Shebang_(Unix))
+
+# make empty directories in your git repo
+mkdir -p analysis docs data
+
+# add a README.md to each directory
+# the scripts directory already exists
+for my_directory in scripts analysis docs data;do
+  touch ${my_directory}/README.md
+  echo "# ${my_directory}" >> ${my_directory}/README.md
+done
+
+
